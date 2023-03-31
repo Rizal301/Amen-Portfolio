849c006e554f31b1eb5bbb19072046594a056781
Mamen <amentogu6@gmail.com>
2023-03-31 09:41:35 +0700

    edit dikit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $customurl_yt = $result['items'][0]["snippet"]["customUrl"];
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-    <title>My Portfolio = Amen Tobing</title>
+    <title>My Portfolio - Amen Tobing</title>
     <link rel="stylesheet" href="style.css" />
     <link rel="shortcut icon" href="foto.jpg" type="image/x-icon">
   </head>
